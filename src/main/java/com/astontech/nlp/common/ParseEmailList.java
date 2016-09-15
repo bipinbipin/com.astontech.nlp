49db514c3711362eb4373461dfb7815f91a54ca8
Bipin Butala <bipinbipin@gmail.com>
2016-09-15 14:21:01 -0500

    added ParseEmailList class
    will take in a giant string blob and pull out all emails and remove duplicates.

---
+++ src/main/java/com/astontech/nlp/common/ParseEmailList.java
@@ -0,0 +1,56 @@
+package com.astontech.nlp.common;
+
+import org.apache.tika.Tika;
+
+import java.io.*;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Bipin on 9/15/2016.
+ */
+public class ParseEmailList {
+
+    public static void main(String[] args) {
+        String filePath =  "C:\\Users\\Aston\\Dropbox\\bipin\\Aston\\Odd Jobs\\emailList.txt";
+        addFilesToList(filePath);
+    }
+
+
+    public static void addFilesToList(String filePath)
+    {
+
+        try
+        {
+            HashSet<String> hashSet = new HashSet<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    Matcher m = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+").matcher(line);
+                    while (m.find()) {
+                        hashSet.add(m.group());
+                    }
+                }
+            }
+
+            // iterate the hashset and output
+            for(String str : hashSet) {
+                System.out.println(str);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static String[] splitStringByNewLine(String s)
+    {
+        return s.split("\\r?\\n");
+    }
+
+}
--- src/main/java/com/astontech/nlp/common/WordToTxt.java
+++ src/main/java/com/astontech/nlp/common/WordToTxt.java
@@ -8,6 +8,8 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.tika.Tika;
 
 import java.io.*;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +20,16 @@ public class WordToTxt
 
     public static void main(String[] args)
     {
-        final File folder = new File("C:/Resumes/");
+        final File folder = new File("Z:\\Resumes\\Java");
+
+        Instant start = Instant.now();
         listFilesForFolder(folder);
+        Instant end = Instant.now();
+
+        System.out.println("Execution Time: " + Duration.between(start, end));
+
+
+
 
     }
 
@@ -39,20 +49,20 @@ public class WordToTxt
             }
             else
             {
-                //System.out.println(fileEntry.getName());
-                addFilesToList(fileEntry.getName());
+                //System.out.println(fileEntry.getAbsolutePath());
+                addFilesToList(fileEntry.getAbsolutePath());
 
             }
         }
     }
 
-    public static void addFilesToList(String _newFile)
+    public static void addFilesToList(String filePath)
     {
 
         try
         {
 
-            String filePath = "C:\\Resumes\\"+_newFile;
+            //String filePath = "Z:\\Resumes\\"+_newFile;
             //System.out.println(filePath);
             File file = new File(filePath);
 
@@ -70,28 +80,31 @@ public class WordToTxt
                 {
                     if (line.charAt(0) == '·')
                     {
-                        //linesToWrite.add(line);
-                        //System.out.println(line);
-                        if(line.contains("Java") | line.contains("Spring") | line.contains("Maven"))
-                        {
-                            System.out.println("OOP: " + line);
-                        }
-                        else if(line.contains("Database") || line.contains("database") || line.contains("SQL"))
-                        {
-                            System.out.println("DB: " + line);
-                        }
-                        else if(line.contains("REST") | line.contains("RESTful"))
-                        {
-                            System.out.println("WS: " + line);
-                        }
-                        else if(line.contains("Bootstrap") | line.contains("Javascript"))
-                        {
-                            System.out.println("FE: " + line);
-                        }
-                        else
-                        {
-                            System.out.println("NC: " + line);
-                        }
+                        String parsedLine = line.substring(2);
+                        System.out.println(parsedLine);
+
+//                        //linesToWrite.add(line);
+//                        //System.out.println(line);
+//                        if(line.contains("Java") | line.contains("Spring") | line.contains("Maven"))
+//                        {
+//                            System.out.println("OOP: " + line);
+//                        }
+//                        else if(line.contains("Database") || line.contains("database") || line.contains("SQL"))
+//                        {
+//                            System.out.println("DB: " + line);
+//                        }
+//                        else if(line.contains("REST") | line.contains("RESTful"))
+//                        {
+//                            System.out.println("WS: " + line);
+//                        }
+//                        else if(line.contains("Bootstrap") | line.contains("Javascript"))
+//                        {
+//                            System.out.println("FE: " + line);
+//                        }
+//                        else
+//                        {
+//                            System.out.println("NC: " + line);
+//                        }
                     }
                 }
             }
